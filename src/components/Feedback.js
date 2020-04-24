b3510892537a55cedbe50bfa137801fbd391a6c9
Daniil Boyko <daniil09052000@mail.ru>
2020-04-24 15:41:29 +0300

    Create Feedback,Callback and OurWorks components

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,98 +1,94 @@
 import React, { Component } from 'react'
-import { Table, Popconfirm, message, Tag } from 'antd';
+ import { Table, Popconfirm, message, Tag } from 'antd';
 
-const data = [
-		{
-			key: 1,
-			id: 1,
-			name: 'Иван Иванов',
-			status: true,
-			feedback: 'Отличныя работа, буду всем советовать',
-		},
-		{
-			key: 2,
-			id: 2,
-			name: 'Петр Петров',
-			status: false,
-			feedback: 'Сделали крутой ремонт, спасибо большое',
-		},
-		{
-			key: 3,
-			id: 3,
-			name: 'Вася Смирнов',
-			status: true,
-			feedback: 'Быстро качетвенно, не дорого',
-		}
-]
+ export default class Feedback extends Component {
+ 	constructor(props) {
+ 		super(props)
 
-export default class Feedback extends Component {
-	constructor(props) {
-		super(props)
+ 		this.state = {
+ 			data : [
+ 				{
+ 					key: 1,
+ 					id: 1,
+ 					name: 'Иван Иванов',
+ 					status: true,
+ 					feedback: 'Отличныя работа, буду всем советовать',
+ 				},
+ 				{
+ 					key: 2,
+ 					id: 2,
+ 					name: 'Петр Петров',
+ 					status: false,
+ 					feedback: 'Сделали крутой ремонт, спасибо большое',
+ 				},
+ 				{
+ 					key: 3,
+ 					id: 3,
+ 					name: 'Вася Смирнов',
+ 					status: true,
+ 					feedback: 'Быстро качетвенно, не дорого',
+ 				},
+ 			] 
+ 		}
 
-		this.state = {
-		}
+ 		this.columns = [
+ 				{ title: 'id', dataIndex: 'id', key: 'id' },
+ 				{ title: 'Имя', dataIndex: 'name', key: 'name' },
+ 				{ title: 'Отзыв', dataIndex: 'feedback', key: 'feedback' },
+ 				{
+ 					title: 'Статус',
+ 					dataIndex: 'status',
+ 					key: 'status',
+ 					render: val => val
+  					? <Tag color="success">Активен</Tag>
+  					: <Tag color="default">Не активен</Tag>
+ 				},
+ 				{
+ 					title: 'Действия',
+ 					dataIndex: '',
+ 					key: 'x',
+ 					render: (record) => 
+ 					this.state.data.length >= 1 ? (
+ 						<div>
+ 						<Popconfirm title="Вы уверены что хотите удалить?" okText="Да" cancelText="Нет"
+ 						onConfirm={() => this.handleDelete(record.key)}>
+ 								<a href="/feedback">Удалить</a>
+ 						</Popconfirm>
+ 						<Popconfirm title="Опубликовать отзыв на сайте?" okText="Да" cancelText="Нет"
+ 							onConfirm={() => this.handleTweet()}>
+ 								<a href="/feedback" className='feedback__link'>Опубликовать</a>
+ 						</Popconfirm>
+ 					</div>
+ 					) : null,
+ 				},
+ 			];
 
-		this.columns = [
-				{ title: 'id', dataIndex: 'id', key: 'id' },
-				{ title: 'Имя', dataIndex: 'name', key: 'name' },
-				{ title: 'Отзыв', dataIndex: 'feedback', key: 'feedback' },
-				{
-					title: 'Статус',
-					dataIndex: 'status',
-					key: 'status',
-					// render: () => {
-					// 	this.items = this.data.map((item, key) => 
-					// 		// console.log(item.status)
-					// 		item.status ?  <Tag color="success">Активен</Tag>  : <Tag color="default">Не активен</Tag>
-					// 	)
-					// }
-				},
-				{
-					title: 'Действия',
-					dataIndex: '',
-					key: 'x',
-					// render: (record) => 
-					// this.data.length >= 1 ? (
-					// 	<div>
-					// 	<Popconfirm title="Вы уверены что хотите удалить?" okText="Да" cancelText="Нет"
-					// 	onConfirm={() => this.handleDelete(record.key)}>
-					// 			<a href="/feedback">Удалить</a>
-					// 	</Popconfirm>
-					// 	<Popconfirm title="Разместить отзыв на сайте?" okText="Да" cancelText="Нет"
-					// 	onConfirm={() => this.handleTweet()}>
-					// 			<a href="/feedback" className='feedback__link'>Опубликовать</a>
-					// 	</Popconfirm>
-					// </div>
-					// ) :null,
-				},
-			];
+ 	}
 
-	}
+ 	handleDelete = key => {
+ 		const data = [...this.state.data];
+ 		this.setState({ data: data.filter(item => item.key !== key) });
+ 		message.warning('Отзыв удален');
+	 };
 
-	handleDelete = key => {
-		const data = [...this.state.data];
-		this.setState({ data: data.filter(item => item.key !== key) });
-		message.warning('Отзыв удален');
-	};
+ 	handleTweet = () => { 
+ 		message.success('Отзыв опубликован!')	
+ 	}
 
-	handleTweet = () => { 
-		message.success('Отзыв опубликован!')	
-	}
+ 	render() {
+ 		const { data } = this.state;
+ 		const columns = this.columns.map(col => {  return col; } )
 
-	render() {
-		// const { data } = this.state;
-		const columns = this.columns.map(col => {  return col; } )
-      
-    
-		return (
-			<div>
-				<h2>Отзывы</h2>
-				<Table columns={columns}
-							 pagination={false}
-    					 expandable={{
-               rowExpandable: record => record.name !== 'Not Expandable',}}
-               dataSource={data}/>
-			</div>
-		)
-	}
-}
+
+ 		return (
+ 			<div>
+ 				<h2>Отзывы</h2>
+ 				<Table columns={columns}
+ 							 pagination={false}
+     					 expandable={{
+                rowExpandable: record => record.name !== 'Not Expandable',}}
+                dataSource={data}/>
+ 			</div>
+ 		)
+ 	}
+ }
--- src/components/OurWorks.js
+++ src/components/OurWorks.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react'
+import { Card } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
 
 export default class OurWorks extends Component {
 	render() {
 		return (
 			<div>
 				<h2>Наши работы</h2>
+				<div className="works">
+					<Card hoverable
+							style={{ width: 240 }} className="work__card" 
+							actions={[
+								<EditOutlined key="edit" />,
+								<DeleteOutlined key="delete"  onClick={() => this.handleDelete()}/>,
+							]}
+    					cover={<img alt="example" src="https://static.tildacdn.com/tild3163-6462-4237-b263-386637376164/DSC_0646_1.jpg" />}>
+   			 		<Meta title="Новочеркаск" description="Студия" />
+  				</Card>
+					<Card hoverable
+							style={{ width: 240 }} className="work__card" key="2"
+							actions={[
+								<EditOutlined key="edit" />,
+								<DeleteOutlined key="delete" />,
+							]}
+    					cover={<img alt="example" src="https://static.tildacdn.com/tild3539-3435-4933-a464-346533383132/1_37.JPG" />}>
+   			 		<Meta title="Аксай" description="Студия" />
+  				</Card>
+					<Card hoverable
+							style={{ width: 240 }} className="work__card" key="3"
+							actions={[
+								<EditOutlined key="edit" />,
+								<DeleteOutlined key="delete" />,
+							]}
+    					cover={<img alt="example" src="https://grand-remont.com/images/remont_kvartir_spb_15.jpg" />}>
+   			 		<Meta title="Ростов" description="Студия" />
+  				</Card>
+					<Card hoverable
+							style={{ width: 240 }} className="work__card" key="4"
+							actions={[
+								<EditOutlined key="edit" />,
+								<DeleteOutlined key="delete" />,
+							]}
+    					cover={<img alt="example" src="https://i.simpalsmedia.com/999.md/BoardImages/900x900/e687a6a5288e877e4720f2c430e06c3f.jpg" />}>
+   			 		<Meta title="Ростов" description="Спальня" />
+  				</Card>
+					<Card hoverable
+							style={{ width: 240 }} className="work__card" key="5"
+							actions={[
+								<EditOutlined key="edit" />,
+								<DeleteOutlined key="delete" />,
+							]}
+    					cover={<img alt="example" src="https://static.tildacdn.com/tild3163-6462-4237-b263-386637376164/DSC_0646_1.jpg" />}>
+   			 		<Meta title="Новочеркаск" description="Студия" />
+  				</Card>
+				</div>
 			</div>
 		)
 	}
