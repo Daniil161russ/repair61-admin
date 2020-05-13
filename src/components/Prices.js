64f1a0fd09f9f0a5fea1c9217951035b61c9692b
Daniil Boyko <daniil09052000@mail.ru>
2020-05-14 00:57:19 +0300

    Получение таблицы цен на покрытия из бд  и изменения их

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,5 +1,6 @@
 import React, { Component, useContext, useState, useEffect, useRef } from 'react'
-import { Table, InputNumber, Form } from 'antd';
+import axios from 'axios'
+import { Table, InputNumber, Form, message } from 'antd';
 
 const EditableContext = React.createContext();
 
@@ -66,7 +67,6 @@ const EditableCell = ({
         ]}
       >
         <InputNumber ref={inputRef} onPressEnter={save} onBlur={save}/>
-        {/* <Input /> */}
       </Form.Item>
     ) : (
       <div className="editable-cell-value-wrap"
@@ -87,102 +87,62 @@ export default class Prices extends Component {
 		super(props)
 
 		this.columns = [
-			{ title: 'id', dataIndex: 'id', key: 'id' },
-			{ title: 'Покрытие', dataIndex: 'coating', key: 'coating' },
+			{ title: 'id', dataIndex: 'id_price', key: 'id_price' },
+			{ title: 'Покрытие', dataIndex: 'cover', key: 'cover' },
 			{ title: 'Цена за 1 м2 в рублях', dataIndex: 'price', key: 'price' ,editable: true, },
     ];
 
     this.state = {
-      dataSource: [
-        {
-          key: '0',
-          id: '1',
-          coating: 'Ламинат',
-          price: 100,
-        },
-        {
-          key: '1',
-          id: '2',
-          coating: 'Паркетная доска',
-          price: 200,
-				},
-				{
-          key: '2',
-          id: '3',
-          coating: 'Керамогранит',
-					price: 350,
-				},
-				{
-          key: '3',
-          id: '4',
-          coating: 'Линолеум',
-					price: 160,
-				},
-				{
-          key: '4',
-          id: '5',
-          coating: 'Наливные полы',
-					price: 500,
-				},
-				{
-          key: '5',
-          id: '6',
-          coating: 'Краснка',
-					price: 220,
-				},
-				{
-          key: '6',
-          id: '7',
-          coating: 'Обои',
-					price: 300,
-				},
-				{
-          key: '7',
-          id: '8',
-          coating: 'Декор. штукатурка',
-					price: 350,
-				},
-				{
-          key: '8',
-          id: '9',
-          coating: 'Плитка',
-					price: 300,
-				},
-				{
-          key: '9',
-          id: '10',
-          coating: 'Натяжные потолки',
-					price: 200,
-				},
-				{
-          key: '10',
-          id: '11',
-          coating: 'Гипсокартон 1 ур.',
-					price: 400,
-				},
-				{
-          key: '11',
-          id: '12',
-          coating: 'Гипсокартон 2-3 ур.',
-					price: 650,
-        },
-      ],
+      prices: []
 		};
+  }
+  
+  componentDidMount() {
+		const config = {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('api_token')}`
+			}
+		};
+		axios.get('https://api.rostovrepair161.ru/api/prices', config)
+		.then( response => {
+			this.setState({
+				prices: response.data['prices'],
+			});
+		})
 	}
 
-  handleSave = row => {
-    const newData = [...this.state.dataSource];
-    const index = newData.findIndex(item => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {
-      ...item,
-      ...row,
-    });
-    this.setState({ dataSource: newData });
+  handleSave = (row) => {
+    const priceObj = {
+			price: row.price
+		}
+
+    const config = {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('api_token')}`
+			}
+		};
+    axios.post(`https://api.rostovrepair161.ru/api/edit-price/${row.id_price}`, priceObj, config)
+    .then( response => {
+			if (response.status === 200) {
+				const newData = [...this.state.prices];
+        const index = newData.findIndex(item => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        this.setState({ prices: newData });
+				message.success('Цена изменена!')
+			}
+		})
   };
+
+  getRandom() {
+		return Math.random();
+	}
 	
 	render() {
-		const { dataSource } = this.state;
+		const { prices } = this.state;
     const components = {
       body: {
         row: EditableRow,
@@ -210,10 +170,11 @@ export default class Prices extends Component {
 			<div>
         <Table
           components={components}
+          rowKey={this.getRandom}
           rowClassName={() => 'editable-row'}
 					bordered
 					pagination={false}
-          dataSource={dataSource}
+          dataSource={prices}
           columns={columns}
         />
       </div>
